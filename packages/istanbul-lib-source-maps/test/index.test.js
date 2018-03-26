69c52672cfeb0e5aad0a3c630b39030e123a043f
Pedro Carriço <pedro.carrico@gmail.com>
2018-03-26 16:15:27 +0100

    (testing)

--- packages/istanbul-lib-source-maps/test/index.test.js
+++
@@ -1,9 +0,0 @@
-/* globals describe, it */
-var assert = require('chai').assert,
-    index = require('../index');
-
-describe('exports', function () {
-    it('exports the correct interface', function () {
-        assert.isObject(index);
-    });
-});
